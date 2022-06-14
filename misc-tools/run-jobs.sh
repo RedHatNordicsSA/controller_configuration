02da24f645c85f20d6dc7595d73d701b78b1ab9a
open.grieves@gmail.com <sudo@redhat.com>
2022-06-14 16:17:30 +0200

    runs all job templates

---
+++ misc-tools/run-jobs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Script which triggers sync of job templates from outside of AAP
+# Magnus Glantz, sudo@redhat.com, 2022
+
+# Source token from .token.cfg file
+# If it doesn't exist, create file containing: TOKEN="your token"
+. ~/.token.cfg
+
+# Number of job id's we will iterate through, adjust as required
+for i in {1..200}
+do
+	RESULT=""
+	RESULT=$(awx -k --conf.token $TOKEN job_templates get $i)
+	if [ "$?" -eq 0 ]
+	then
+		JOBID=$(echo $RESULT|jq .id)
+		JOBNAME=$(echo $RESULT|jq .name)
+		if echo $JOBNAME|grep -i "Controller Synchronization" >/dev/null
+		then
+			awx -k --conf.token $TOKEN job_templates launch $JOBID --wait >/dev/null
+			if [ "$?" -eq 0 ]; then
+				echo "Ran: $JOBNAME"
+			fi
+		fi
+		if echo $JOBNAME|grep -i "Customer synchronization" >/dev/null
+		then
+			awx -k --conf.token $TOKEN job_templates launch $JOBID >/dev/null
+                        if [ "$?" -eq 0 ]; then
+                                echo "Ran: $JOBNAME"
+			fi
+		fi
+	fi
+done
+
